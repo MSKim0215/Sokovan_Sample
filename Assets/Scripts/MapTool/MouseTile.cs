8e265b78f8fc00634f45d0a80b000e3c2c786e08
MSKim0215 <rlaalstjqu05@gmail.com>
2022-04-04 20:01:01 +0900

    22.04.04 commit #2
    1) 맵툴 사전 작업
    2) 마우스 포인터 위치 타일 색상 변경

---
+++ Assets/Scripts/MapTool/MouseTile.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class MouseTile : MonoBehaviour
+{
+    private Tilemap tilemap;
+
+    private void Awake()
+    {
+        tilemap = GetComponentInParent<Tilemap>();
+    }
+
+    private void OnMouseOver()
+    {
+        try
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit2D hit = Physics2D.Raycast(ray.origin, Vector3.zero);
+            if(this.tilemap = hit.transform.GetComponent<Tilemap>())
+            {
+                this.tilemap.RefreshAllTiles();
+                int x, y;
+                x = this.tilemap.WorldToCell(ray.origin).x;
+                y = this.tilemap.WorldToCell(ray.origin).y;
+                Vector3Int v3Int = new Vector3Int(x, y, 0);
+
+                this.tilemap.SetTileFlags(v3Int, TileFlags.None);
+                this.tilemap.SetColor(v3Int, (Color.red));
+            }
+        }
+        catch(NullReferenceException)
+        {
+        }
+    }
+
+    private void OnMouseExit()
+    {
+        this.tilemap.RefreshAllTiles();
+    }
+}
